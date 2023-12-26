33a6dc29d84b1d1aba1ed6d4d1b09d27e76cd61e
guido2288 <guido2288@gmail.com>
2023-12-26 20:17:59 -0300

    Plan section added

--- src/components/PlanSelect.jsx
+++ src/components/PlanSelect.jsx
@@ -11,15 +11,16 @@ const PlanSelect = () => {
         <div className='plan_selection'>
         
             <img src="./images/icon-arcade.svg" alt="arcade" />
-            <div>
+            <div className='plan_price'>
                 <p>Arcade</p>
                 <span>$9/mo</span>
+                
             </div>
         </div>
 
         <div className='plan_selection'>
             <img src="./images/icon-advanced.svg" alt="advanced" />
-        <div>
+        <div className='plan_price'>
             <p>Advanced</p>
             <span>$12/mo</span>
         </div>
@@ -27,13 +28,20 @@ const PlanSelect = () => {
 
       <div className='plan_selection'>
         <img src="./images/icon-pro.svg" alt="pro" />
-        <div>
+        <div className='plan_price'>
             <p>Pro</p>
             <span>$15/mo</span>
         </div>
       </div>
 
+
     </form>
+
+    <div className='switch_container'>
+        <p>Monthly</p>
+        <input type="checkbox" id="switch" /><label htmlFor="switch" className='switch_label'></label>
+        <p>Yearly</p>
+      </div>
 
 
     </section>
