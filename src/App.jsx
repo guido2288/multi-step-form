4a7967ced4676a45ca99bfbdb3bb448fb72eea2a
guido2288 <guido2288@gmail.com>
2024-01-28 13:54:50 -0300

    Fix vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import Laststep from './components/Laststep'
-import PersonalInfo from './components/PersonalInfo'
-import PickAdd from './components/PickAdd'
-import PlanSelect from './components/PlanSelect'
 import Steps from './components/Steps'
-import Summary from './components/Summary'
 import './App.css'
 import RegFormProvider from './provider/RegFormProvider'
 
